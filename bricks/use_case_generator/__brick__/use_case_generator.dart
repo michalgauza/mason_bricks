b4e55cffca5ad33e101581d4eb549e846742b70e
Michał Gauza <michalgauza@Michas-MacBook-Pro-2.local>
2022-05-02 14:27:10 +0200

    update use_case_generator

--- bricks/use_case_generator/__brick__/use_case_generator.dart
+++ bricks/use_case_generator/__brick__/use_case_generator.dart
@@ -2,31 +2,59 @@ import 'dart:io';
 
 import 'package:mason/mason.dart';
 
-import 'use_case_bundle.dart';
+String getRedString(String text) => '\x1B[31m$text\x1B[0m';
 
-String getSuccessString(String text) => '\x1B[92m🎉 $text 🎉\x1B[0m';
+String getGreenString(String text) => '\x1B[92m$text\x1B[0m';
+
+String getGreyString(String text) => '\x1B[37m$text\x1B[0m';
+
+String getBlueString(String text) => '\x1B[94m$text\x1B[0m';
 
 class UseCaseGenerator {
-  final Logger _logger = Logger();
+  static Logger _logger = Logger();
 
-  Future generate({
-    required String path,
-    required String name,
-    required String response,
-    required String request,
-    required String repo,
+  static Future generate({
+    String? defaultPath,
+    String? defaultName,
+    String? defaultResponse,
+    String? defaultRequest,
+    String? defaultRepo,
+    Function(Map<String, String>)? onGenerateCallback,
   }) async {
-    final generator = await MasonGenerator.fromBundle(useCaseBundle);
-    await generator.generate(
-      DirectoryGeneratorTarget(Directory.current),
-      vars: {
-        'path': path,
-        'name': name,
-        'response': response,
-        'request': request,
-        'repo': repo,
+    final vars = {
+      'path': defaultPath ?? '',
+      'name': defaultName ?? '',
+      'response': defaultResponse ?? '',
+      'request': defaultRequest ?? '',
+      'repo': defaultRepo ?? '',
+    };
+
+    vars.forEach(
+          (key, value) {
+        _logger.write('$key: ${getGreyString(value)} ');
+        final input = stdin.readLineSync() ?? '';
+        final newValue = input.isNotEmpty ? input : value;
+        vars.update(
+          key,
+              (_) => newValue,
+          ifAbsent: () => newValue,
+        );
       },
     );
-    _logger.success(getSuccessString('$name created!'));
+    _logger.progress('');
+
+    final brick = await Brick.git(
+      GitPath(
+        'https://github.com/michalgauza/mason_bricks.git',
+        path: 'bricks/use_case',
+      ),
+    );
+    final generator = await MasonGenerator.fromBrick(brick);
+    await generator.generate(
+      DirectoryGeneratorTarget(Directory.current),
+      vars: vars,
+    );
+    onGenerateCallback?.call(vars);
+    _logger.success(getGreenString('use_case created!'));
   }
 }
